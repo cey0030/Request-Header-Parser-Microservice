d459d93646486b6f417f7b915a60e70198ccbcd6
Icy <48426058+cey0030@users.noreply.github.com>
2020-06-23 14:16:08 +1200

    Added comment

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.get("/", function (req, res) {
 app.get("/api/whoami", function (req, res) {
   res.json({"ipaddress": req.headers["x-forwarded-for"], "language": req.headers["accept-language"], "software": req.headers["user-agent"]});
 });
+// End of challenge code
 
 
 
